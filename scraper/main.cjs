1936b399bb27255964e47033aeea21eed890a7fe
Kaeko <cuvierclement@proton.me>
2024-10-06 21:21:32 +0200

    Fix js join error and undifined pagination

--- scraper/main.cjs
+++ scraper/main.cjs
@@ -1,13 +1,23 @@
 // Imports
 const https = require("https");
 const { Kafka } = require('kafkajs');
-const fs = require('fs');
+const process = require('node:process');
+
+const defaultBrokers = ["localhost:29092", "localhost:39092", "localhost:49092"];
+const brokers = process.env.KAFKA_BROKERS ? process.env.KAFKA_BROKERS.split(",") : defaultBrokers;
+
+const defaultRequestInterval = 1000 * 60 * 5; // 5 minutes
+const requestInterval = process.env.REQUEST_INTERVAL_SECONDS ? parseInt(process.env.REQUEST_INTERVAL_SECONDS) * 1000 : defaultRequestInterval;
+
+console.log("Starting data scrapper service");
+console.log("Brokers: ", brokers);
+console.log(`Request interval: ${requestInterval}ms`);
 
 const kafka = new Kafka({
 	ssl: false, // optional, defaults to false
 	// Define the brokers (or list of brokers)
 	clientId: 'Scapper Twitch',
-	brokers: ['localhost:29092', 'localhost:39092', 'localhost:49092'],
+	brokers,
 	socketTimeout: 30000, // Set a longer socket timeout (30 seconds)
 	maxInFlightRequests: 1000, // Increase the number of in-flight requests
 });
@@ -48,9 +58,8 @@ var token = "";
 var dictionary = {};
 var mem = [];
 var games = [];
-var process = false;
 var last = null;
-function getTopGames(pagination, refresh = false) {
+function getTopGames(pagination) {
 	var toppath = "/helix/games/top?first=100";
 	if (pagination !== null) {
 		toppath += "&after=" + pagination;
@@ -64,6 +73,7 @@ function getTopGames(pagination, refresh = false) {
 			"Client-Id": "hjkqp89ai189tpqz80ysz2lg1mymy3",
 		},
 	};
+	const timestamp = Date.now();
 	Promise.all([
 		https
 			.get(getOptions, (res) => {
@@ -75,8 +85,8 @@ function getTopGames(pagination, refresh = false) {
 
 				res.on("end", () => {
 					var response = JSON.parse(data).data;
-					if (response == undefined && !refresh) {
-						lookupStreams();	
+					if (response == undefined) {
+						lookupStreams(timestamp);	
 					} else {
 						response.forEach((game) => {
 							if (games.indexOf(game.id) === -1) {
@@ -91,8 +101,12 @@ function getTopGames(pagination, refresh = false) {
 			})
 	])
 }
-async function getStreams(pagination, gameid) {
+async function getStreams(pagination, gameid, timestamp) {
 	let batch = false;
+	if (Array.isArray(gameid)) {
+		gameid = gameid.filter(id => id !== '');
+	}
+	// console.log(gameid);
 	if (typeof(gameid) === "string") {
 		gameid = "&game_id=" + gameid;
 		batch = false;
@@ -110,7 +124,7 @@ async function getStreams(pagination, gameid) {
 	}
 	// console.log(gameid);
 	var path = "/helix/streams?first=100&type=live" + gameid;
-	if (pagination !== null) {
+	if (pagination !== null && pagination !== undefined) {
 		path += "&after=" + pagination;
 	}
 	const getOptions = {
@@ -122,7 +136,6 @@ async function getStreams(pagination, gameid) {
 			"Client-Id": "hjkqp89ai189tpqz80ysz2lg1mymy3",
 		},
 	};
-	let ping = Date.now();
 	Promise.all([
 		https
 			.get(getOptions, (res) => {
@@ -149,7 +162,7 @@ async function getStreams(pagination, gameid) {
 						for (const gameId in groupedStreams) {
 							const gameStreams = groupedStreams[gameId];
 							let GameName = "";
-							message = `${ping};${games.indexOf(gameId)};${games.length}\n`;
+							message = `${timestamp};${games.indexOf(gameId)};${games.length}\n`;
 							gameStreams.forEach((stream) => {
 								GameName = stream.game_name;
 								message += `${stream.id};${stream.user_id};${stream.game_id};${stream.language};${stream.viewer_count}\n`;
@@ -167,36 +180,32 @@ async function getStreams(pagination, gameid) {
 						}
 						if (JSON.parse(data).pagination.cursor !== null) {
 							pagination = JSON.parse(data).pagination.cursor;
-							getStreams(pagination, gameid.split("&game_id="));
+							getStreams(pagination, gameid.split("&game_id="), timestamp);
 						} 
 						return;
 					} else if (JSON.parse(data).status === 429) {
-						const logStream = fs.createWriteStream('stream_logs.txt', { flags: 'a' });
-						logStream.write(`Rate limit exceeded\n`);
-						console.log("Rate limit exceeded");
+						// console.log("Rate limit exceeded");
 						setTimeout(() => {
-							getStreams(pagination ? pagination:null, gameid.split("&game_id="));
+							getStreams(pagination ? pagination:null, gameid.split("&game_id="), timestamp);
 						}, 1000 * 1);
-					}
+					} 
 			});
 		})
 	])
 }
 
-function lookupStreams() {
+function lookupStreams(timestamp) {
 	console.log("Lookup streams on " + games.length + " games");
-	const logStream = fs.createWriteStream('stream_logs.txt', { flags: 'a' });
-	logStream.write(`Lookup streams on ${games.length} games at ${new Date().toLocaleString()}\n`);
 	games.slice(0, 50).forEach((game) => {
-		getStreams(null, game);
+		getStreams(null, game, timestamp);
 	});
 	let group = [];
 	games.slice(50).forEach((game, index) => {
-		if (index % 22 !== 0) {
+		if (index % 100 !== 0) {
 			group.push(game);
 		} else {
 			if (group.length !== 0) {
-				getStreams(null, group);
+				getStreams(null, group, timestamp);
 				group = [];
 			}	
 		}
@@ -223,4 +232,4 @@ run().catch(console.error);
 setInterval(() => {
 	console.log("new run : " + new Date().toLocaleTimeString());
 	run().catch(console.error);
-}, 1000 * 60);
+}, requestInterval);
