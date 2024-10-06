e93cabed7738262c0694db6e87e835164e5bd26d
Frédéric Hilleriteau <46034382+hilleriteauf@users.noreply.github.com>
2024-10-06 17:42:31 +0200

    Dockerisation traitement des données

--- data-processing/index.ts
+++ data-processing/index.ts
@@ -2,13 +2,20 @@ import { Kafka } from "kafkajs";
 import { EventHandler } from "./eventHandler";
 import { parseScrapperEvent } from "./event";
 
+const defaultBrokers = ["localhost:29092", "localhost:39092", "localhost:49092"];
+const brokers = process.env.KAFKA_BROKERS ? process.env.KAFKA_BROKERS.split(",") : defaultBrokers;
+
+console.log("Starting data processing service with brokers: ", brokers);
+
 const kafka = new Kafka({
   clientId: "data-processing",
-  brokers: ["localhost:29092", "localhost:39092", "localhost:49092"],
+  brokers,
+  connectionTimeout: 3000,
+  retry: { retries: 10 },
 });
 
 // Listen for the "data" topic
-const consumer = kafka.consumer({ groupId: "data-processing" });
+const consumer = kafka.consumer({ groupId: "data-processing", allowAutoTopicCreation: true });
 consumer.connect();
 consumer.subscribe({ topic: "twitch-streams", fromBeginning: true });
 
